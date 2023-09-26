49da72fe08b7de06bea7140036e891d7ff692be2
aansyawaludddin <aansyawaluddin22122003@gmai.com>
2023-09-26 14:09:01 +0800

    pertemuan 3

---
+++ H071221031/pertemuan-3/Tambahan.sql
@@ -0,0 +1,8 @@
+SELECT * FROM employees
+
+DESCRIBE employees
+
+INSERT INTO employees(employeeNumber, lastName, firstName, extension, email, officeCode, reportsTo, jobTitle)
+VALUES ( 6792, "Mina", "Myoi", "X67638", "minaTwice@classicmodelcars.com", 1, 1002, "Dancer"),
+		 ( 10, "Jihyo", "Kim", "X67618", "jihyoTwice@classicmodelcars.com", 1, 1002, "Dancer"),
+		 ( 112, "Syawaluddin", "Aan", "X61688", "aansyawaluddin@classicmodelcars.com", 1, 1002, "Runners")
